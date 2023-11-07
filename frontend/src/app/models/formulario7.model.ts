29514e4e01501619b88f198ab599770dffe15c44
luxho24 <202111482@urp.edu.pe>
2023-11-07 12:46:53 -0500

    Registro de todos los formularios COMPLETADO
    Se realizo la codificacion de la funcionalidad de registrar pacientes en todos los formularios al 100%.
    
    NOTA: Para editar un formulario como superadmin falta completar el formulario 6, adicionalmente falta ponerle validaciones en el frontend y tambien alertas con "sweetalert2" y "toast" (dependencia de angular)

--- frontend/src/app/models/formulario7.model.ts
+++ frontend/src/app/models/formulario7.model.ts
@@ -4,7 +4,27 @@ export class Formulario7 {
     numero_hc: string;
     centro_institucion_atencion: string;
 
-    esquema_tratamiento: string;
+    tratamiento_medicamento_1: string;
+    tratamiento_dosis_1: string;
+    tratamiento_medicamento_2: string;
+    tratamiento_dosis_2: string;
+    tratamiento_medicamento_3: string;
+    tratamiento_dosis_3: string;
+    tratamiento_medicamento_4: string;
+    tratamiento_dosis_4: string;
+    tratamiento_medicamento_5: string;
+    tratamiento_dosis_5: string;
+    tratamiento_medicamento_6: string;
+    tratamiento_dosis_6: string;
+    tratamiento_medicamento_7: string;
+    tratamiento_dosis_7: string;
+    tratamiento_medicamento_8: string;
+    tratamiento_dosis_8: string;
+    tratamiento_medicamento_9: string;
+    tratamiento_dosis_9: string;
+    tratamiento_medicamento_10: string;
+    tratamiento_dosis_10: string;
+
     fecha_tratamiento: string;
     ciclo: string;
     tolerancia: string;
@@ -19,7 +39,27 @@ export class Formulario7 {
         numero_hc: string,
         centro_institucion_atencion: string,
     
-        esquema_tratamiento: string,
+        tratamiento_medicamento_1: string,
+        tratamiento_dosis_1: string,
+        tratamiento_medicamento_2: string,
+        tratamiento_dosis_2: string,
+        tratamiento_medicamento_3: string,
+        tratamiento_dosis_3: string,
+        tratamiento_medicamento_4: string,
+        tratamiento_dosis_4: string,
+        tratamiento_medicamento_5: string,
+        tratamiento_dosis_5: string,
+        tratamiento_medicamento_6: string,
+        tratamiento_dosis_6: string,
+        tratamiento_medicamento_7: string,
+        tratamiento_dosis_7: string,
+        tratamiento_medicamento_8: string,
+        tratamiento_dosis_8: string,
+        tratamiento_medicamento_9: string,
+        tratamiento_dosis_9: string,
+        tratamiento_medicamento_10: string,
+        tratamiento_dosis_10: string,
+
         fecha_tratamiento: string,
         ciclo: string,
         tolerancia: string,
@@ -34,7 +74,27 @@ export class Formulario7 {
         this.numero_hc = numero_hc;
         this.centro_institucion_atencion = centro_institucion_atencion;
 
-        this.esquema_tratamiento = esquema_tratamiento;
+        this.tratamiento_medicamento_1 = tratamiento_medicamento_1;
+        this.tratamiento_dosis_1 = tratamiento_dosis_1;
+        this.tratamiento_medicamento_2 = tratamiento_medicamento_2;
+        this.tratamiento_dosis_2 = tratamiento_dosis_2;
+        this.tratamiento_medicamento_3 = tratamiento_medicamento_3;
+        this.tratamiento_dosis_3 = tratamiento_dosis_3;
+        this.tratamiento_medicamento_4 = tratamiento_medicamento_4;
+        this.tratamiento_dosis_4 = tratamiento_dosis_4;
+        this.tratamiento_medicamento_5 = tratamiento_medicamento_5;
+        this.tratamiento_dosis_5 = tratamiento_dosis_5;
+        this.tratamiento_medicamento_6 = tratamiento_medicamento_6;
+        this.tratamiento_dosis_6 = tratamiento_dosis_6;
+        this.tratamiento_medicamento_7 = tratamiento_medicamento_7;
+        this.tratamiento_dosis_7 = tratamiento_dosis_7;
+        this.tratamiento_medicamento_8 = tratamiento_medicamento_8;
+        this.tratamiento_dosis_8 = tratamiento_dosis_8;
+        this.tratamiento_medicamento_9 = tratamiento_medicamento_9;
+        this.tratamiento_dosis_9 = tratamiento_dosis_9;
+        this.tratamiento_medicamento_10 = tratamiento_medicamento_10;
+        this.tratamiento_dosis_10 = tratamiento_dosis_10;
+        
         this.fecha_tratamiento = fecha_tratamiento;
         this.ciclo = ciclo;
         this.tolerancia = tolerancia;
