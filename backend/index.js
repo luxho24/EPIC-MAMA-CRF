ffa7a41cc67cb12c4ada105267cb3c309523ce1a
ser1nlu <sergio.inga@urp.edu.pe>
2023-11-13 16:55:06 -0500

    actualizado

--- backend/index.js
+++ backend/index.js
@@ -36,5 +36,5 @@ app.use(function(req, res, next){
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-    console.log(`Server ejecutandose en http://localhost:${PORT}`);
+    console.log(`Server ejecutandose en http://34.224.140.51:${PORT}`);
 })
